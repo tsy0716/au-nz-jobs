10e276d4a27039e05f89a5e7a90ca84da7a033e2
tsy0716 <tsy0716@gmail.com>
2023-03-01 14:46:47 +0800

    Bump version: 0.1.0 → 0.1.1

--- au_nz_jobs/__init__.py
+++ au_nz_jobs/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Robert Tu"""
 __email__ = 'tsy0716@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 # imports
 from au_nz_jobs.downloader import Job, Jobs
--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/tsy0716/au_nz_jobs',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
     long_description_content_type='text/markdown',
 )
